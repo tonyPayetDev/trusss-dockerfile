c964b9e1e5af7db84f9eaf987fcf820d72a2640c
tony <tony.payet.professionnel@gmail.com>
2025-02-23 16:58:49 +0400

    Update mon_script.py

--- mon_script.py
+++ mon_script.py
@@ -1,1 +1,31 @@
 
+import os
+import random
+import base64
+import requests
+
+# Set essential values
+model_id = "2qjggr2q"
+baseten_api_key = "AUmtSj8s.PbeDv3kJQtZ1XuejV0HrFv4lmS3F8vrp"
+# Set prompts and controlnet image
+values = {
+  "positive_prompt": "A top down view of a river through the woods",
+  "negative_prompt": "blurry, text, low quality",
+  "controlnet_image": "https://storage.googleapis.com/logos-bucket-01/baseten_logo.png",
+  "seed": random.randint(1, 1000000)
+}
+# Call model endpoint
+res = requests.post(
+    f"https://model-{model_id}.api.baseten.co/development/predict",
+    headers={"Authorization": f"Api-Key {baseten_api_key}"},
+    json={"workflow_values": values}
+)
+# Get output image
+res = res.json()
+preamble = "data:image/png;base64,"
+output = base64.b64decode(res["result"][1]["image"].replace(preamble, ""))
+# Save image to file
+img_file = open("comfyui.png", 'wb')
+img_file.write(output)
+img_file.close()
+os.system("open comfyui.png")
